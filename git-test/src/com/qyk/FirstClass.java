b1a6ebe536035ea44e37463cb3a50739982f0482
qiuyangkang <1095382832@qq.com>
2019-10-11 19:54:57 +0800

    idea本地工程添加到git

--- git-test/src/com/qyk/FirstClass.java
+++ git-test/src/com/qyk/FirstClass.java
@@ -2,6 +2,6 @@ package com.qyk;
 
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("hello git!!");
+        System.out.println("hello git111!!");
     }
 }
